3470246b56075d07a458ade59257f06de86940b8
Gusttavobarcell <gustavorodrigueslima5@gmail.com>
2023-11-23 22:58:05 -0300

    att

--- APP/script.js
+++ APP/script.js
@@ -12,7 +12,17 @@ function mudarImagem(caminhoImagem) {
     event.currentTarget.classList.add('selecionado');
 }
 
+// const comentarios = document.getElementById("comentarios");
 
+// // Verifica se a div está vazia
+// if (comentarios.innerHTML.trim() === "") {
+//     // Se estiver vazia, define a propriedade display como "none"
+//     comentarios.style.display = "none";
+// } else {
+//     // Se não estiver vazia, mantém ou define a propriedade display como "block" (ou o valor desejado)
+//     comentarios.style.display = "block"; // ou qualquer outro valor, dependendo do seu layout
+// }
+    
 
 function exibirFormulario() {
     const formularioOverlay = document.getElementById("formularioOverlay");
@@ -45,13 +55,33 @@ function adicionarComentario() {
     const nome = document.getElementById("nome").value;
     const avaliacao = document.getElementById("avaliacao").value;
     const comentario = document.getElementById("comentario").value;
+    const imagemInput = document.getElementById("imagem");
 
     if (nome && avaliacao && comentario) {
         const comentariosContainer = document.getElementById("comentarios");
+        
 
         const novoComentario = document.createElement("div");
         novoComentario.className = "comentario";
-        novoComentario.innerHTML = `<strong>${nome}</strong> - Avaliação: ${avaliacao}/5<br>${comentario}`;
+        // Adicionar a imagem, se uma for selecionada
+        if (imagemInput.files.length > 0) {
+            const imagemURL = URL.createObjectURL(imagemInput.files[0]);
+            novoComentario.innerHTML += `
+            <div class="user-info">
+            <!--imagem do perfil do usuario-->
+            <img class="user-photo" src="${imagemURL}" alt="Foto de perfil">
+            <div class="name_star">
+                <div class="user-name">${nome}</div>
+                <!--avaliação em estrela-->
+                <div class="star-rating">${avaliacao}★★★★★</div>
+            </div>
+            <button class="like-button">👍</button>
+        </div>
+        <!--aonde vai ficar o comentário do usuario-->
+        <div class="comment-text">${comentario}
+        </div>`;
+        }
+
 
         comentariosContainer.appendChild(novoComentario);
 
@@ -59,6 +89,7 @@ function adicionarComentario() {
         document.getElementById("nome").value = "";
         resetEstrelas();
         document.getElementById("comentario").value = "";
+        imagemInput.value = "";
 
         // Fechar o formulário após adicionar um comentário
         fecharFormulario();
