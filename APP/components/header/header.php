837baa142a333595690acec933874ffda33b9f4f
Luizx11978 <luizzz.z@outlook.com>
2023-11-28 16:44:17 -0300

    add: Header&Footer
    add cabeçalho e rodapé

---
+++ APP/components/header/header.php
@@ -0,0 +1,107 @@
+
+<body>
+    <header>
+        <link rel="stylesheet" href="./components/header/header.css?v=<?php echo time(); ?>">
+       <div id="top-header">
+            <p>Loja De Eletrodomésticos em Barreiras Centro, Barreiras - BA</p>
+       </div>
+       <div id="central-header" >
+            
+            
+            <div id="components-central">
+            
+                <div id="logo-header"><img src="./components/header/logo.png" alt="logo" id="logo"></div>
+
+                <section id="esquerda">
+                    <div id="btn-categorias">
+                        <p>Todas as Categorias</p>
+                        <svg width="20" height="20" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M19.5566 9.77827L13.0377 16.2971L6.51885 9.77827" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+
+                    </div>
+                </section>
+                <section id="central">
+                    <div id="input-container">
+                        <form action="">
+                            <input type="search" name="search" id="input-pesquisa">
+                            <label for="search" id="icone-search">
+                                <svg width="17" height="17" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8.11159 15.2857C12.0392 15.2857 15.2232 12.0877 15.2232 8.14285C15.2232 4.19796 12.0392 1 8.11159 1C4.18397 1 1 4.19796 1 8.14285C1 12.0877 4.18397 15.2857 8.11159 15.2857Z" fill="white" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M13.4456 13.5L17.5941 17.6667L13.4456 13.5Z" fill="white"/>
+                                <path d="M13.4456 13.5L17.5941 17.6667" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </label>
+                        </form>
+                    </div>
+                    
+                    <div id="cadastro">
+                        <div class="btn-header">
+                            <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.1666 12.1605C12.25 12.1605 14.3333 9.69136 14.3333 6.60494C14.3333 3.51852 12.9444 1.66667 10.1666 1.66667C7.38887 1.66667 5.99998 3.51852 5.99998 6.60494C5.99998 9.69136 8.08331 12.1605 10.1666 12.1605ZM10.1666 12.1605C14.3333 12.1605 18.5 12.7778 18.5 18.3333H1.83331C1.83331 12.7778 5.99998 12.1605 10.1666 12.1605Z" stroke="black"  stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <div id="btn-paragrafo">
+                            <p><strong>Entre</strong> ou</p>
+                            <p><strong>Cadastre-se</strong></p>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="direita">
+                        <div class="btn-header" id="svg-heart">
+                        <svg width="34" height="32" viewBox="0 0 34 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g filter="url(#filter0_d_4_71)">
+                            <path d="M7.6009 10.09C5.34742 7.0854 6.09858 2.57844 9.85438 1.07612C13.6102 -0.426203 15.8637 2.57844 16.6148 4.08076C17.366 2.57844 20.3706 -0.426203 24.1264 1.07612C27.8822 2.57844 27.8822 7.0854 25.6287 10.09C23.3753 13.0947 16.6148 19.104 16.6148 19.104C16.6148 19.104 9.85438 13.0947 7.6009 10.09Z" fill="#FF0303"/>
+                            <path d="M7.6009 10.09C5.34742 7.0854 6.09858 2.57844 9.85438 1.07612C13.6102 -0.426203 15.8637 2.57844 16.6148 4.08076C17.366 2.57844 20.3706 -0.426203 24.1264 1.07612C27.8822 2.57844 27.8822 7.0854 25.6287 10.09C23.3753 13.0947 16.6148 19.104 16.6148 19.104C16.6148 19.104 9.85438 13.0947 7.6009 10.09Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+                            </g>
+                            <defs>
+                            <filter id="filter0_d_4_71" x="0.235571" y="0.166504" width="33.0311" height="31.0375" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                            <feOffset dy="6"/>
+                            <feGaussianBlur stdDeviation="2.8"/>
+                            <feComposite in2="hardAlpha" operator="out"/>
+                            <feColorMatrix type="matrix" values="0 0 0 0 0.03285 0 0 0 0 0.043065 0 0 0 0 0.135 0 0 0 0.5 0"/>
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_4_71"/>
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_4_71" result="shape"/>
+                            </filter>
+                            </defs>
+                        </svg>
+
+                        </div>
+                        <div class="btn-header">
+                            <svg width="23" height="19" viewBox="0 0 23 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4.53138 4.04762H21.8246L19.663 11.7857H6.69303M19.663 14.1667H7.41358L3.81082 1.66667H1.64917" fill="#F8F5E7"/>
+                                <path d="M4.53138 4.04762H21.8246L19.663 11.7857H6.69303M19.663 14.1667H7.41358L3.81082 1.66667H1.64917" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M18.2219 17.7381C19.0178 17.7381 19.663 17.2051 19.663 16.5476C19.663 15.8901 19.0178 15.3571 18.2219 15.3571C17.426 15.3571 16.7808 15.8901 16.7808 16.5476C16.7808 17.2051 17.426 17.7381 18.2219 17.7381Z" fill="#F8F5E7" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M8.85474 17.7381C9.65064 17.7381 10.2958 17.2051 10.2958 16.5476C10.2958 15.8901 9.65064 15.3571 8.85474 15.3571C8.05884 15.3571 7.41364 15.8901 7.41364 16.5476C7.41364 17.2051 8.05884 17.7381 8.85474 17.7381Z" fill="#F8F5E7" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <div class="btn-header">
+                        <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.1666 12.1605C12.2499 12.1605 14.3333 9.69136 14.3333 6.60494C14.3333 3.51852 12.9444 1.66667 10.1666 1.66667C7.38881 1.66667 5.99992 3.51852 5.99992 6.60494C5.99992 9.69136 8.08325 12.1605 10.1666 12.1605ZM10.1666 12.1605C14.3333 12.1605 18.4999 12.7778 18.4999 18.3333H1.83325C1.83325 12.7778 5.99992 12.1605 10.1666 12.1605Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        </div>
+                </section>
+            </div>
+        </div>
+       </div>
+       <div id="base-header">
+            <div id="nav-base-header">
+                <nav>
+                    <ul>
+                       
+                        <a href="">
+                            <li class="options-header">Home</li>
+                        </a>
+                        <li class="options-header"><a href="">Produtos</a></li>
+                        <li class="options-header"><a href="">Movéis</a></li>
+                        <li class="options-header"><a href="">Eletrodomésticos</a></li>
+                        <li class="options-header"><a href="">Informática</a></li>
+                        <li class="options-header"><a href="">Opção</a></li>
+                    </ul>
+                </nav>
+            </div>
+       </div>
+    </header>
